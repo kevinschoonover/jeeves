da273b33c85d16878c69bff246d115fdbbd7f8c9
Christian Parsons <christianp130@gmail.com>
2019-02-07 21:13:51 -0600

    Wrap App component in a ThemeProvider for ordering and cooking clients

--- clients/ordering/src/index.tsx
+++ clients/ordering/src/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  createMuiTheme,
+  MuiThemeProvider as ThemeProvider,
+} from '@material-ui/core';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
